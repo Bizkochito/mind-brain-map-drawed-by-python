2d2b43da8c4a8e4ffce7bf746d3ef4b899a96255
BPFlow <hupingregoire@hotmail.com>
2023-10-18 09:16:59 +0200

    Implementing st_javascript. Communicating through local storage

--- naotu.py
+++ naotu.py
@@ -1,15 +1,32 @@
 import streamlit as st
 import streamlit.components.v1 as components
 import random
+from streamlit.components.v1 import html
+from streamlit_js_eval import streamlit_js_eval
+from streamlit_javascript import st_javascript
+
+
 st.set_page_config(page_title="可编辑脑图", layout="wide")
+
+def printText():
+   v = st_javascript(
+        "localStorage.getItem('mydate');"
+    )
+   print(v)
+
+if "textInput" not in st.session_state:
+  st.session_state.textInput = ""
+
+
+
 p = open("naotu.html", encoding="utf-8")
-components.html(p.read(), height=1000, width=1800, scrolling=True)
-choose = st.sidebar.selectbox("是否要进行截图保存", ("是", "否"))
-if choose == "是":
-  import PIL.ImageGrab
-  import time
-  time.sleep(3)
-  scr = PIL.ImageGrab.grab()
-  scr.save("screen.png")
-else:
-  pass
+html_component=components.html(p.read(), height=1000, width=1800, scrolling=True)
+
+with st.sidebar:
+  st.button("lalal", on_click=printText)
+
+# with st.sidebar:
+#   html('<div id="mydata"></div>')
+ # Ajoutez un bouton de soumission avec un label
+
+# Vérifiez si le formulaire a été soumis en utilisant le bouton de soumission
